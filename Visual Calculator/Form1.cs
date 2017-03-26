e4a4313fcbfa6a87fe3eba6d13bbbd77e655e225
roockstar <roockstar51@gmail.com>
2017-03-26 18:57:59 +0200

    Add files via upload

---
+++ Visual Calculator/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visual_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWasClicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            string output;
+            if (this.is_operator(btn.Text))
+            {
+                output = " " + btn.Text + " ";
+            }
+            else
+            {
+                output = btn.Text;
+            }
+
+            updateDisplay(output);
+
+        }
+    }
+}
